28c608b468afee8ea0043c0ba73ba94fec8327aa
Bimal Shrestha <69587963+itsbimal@users.noreply.github.com>
2022-07-19 10:11:47 +0545

    Favourite card design

--- src/components/pages/favourite/Favourite.jsx
+++ src/components/pages/favourite/Favourite.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import LeftSide from '../../common/left-side/LeftSide'
+import Sidebar from '../../common/sidebar/Sidebar'
+import './Favourite.css'
 
 const Favourite = () => {
-  return (
-    <div>Favourite</div>
-  )
+    return (
+        <>
+
+            <div class="main-section container">
+                <div class="left-side">
+                    <LeftSide />
+                </div>
+                <div className='middle-side'>
+                    <h4 className='ms-2 mt-2'>Your saved post</h4>
+                    <div class="row row-cols-md-3">
+                        <div class="col">
+                            <div className="favcard">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        This is a longer card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="favcard">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        This is a longer card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div className="favcard">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">
+                                        This is a longer card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default Favourite
