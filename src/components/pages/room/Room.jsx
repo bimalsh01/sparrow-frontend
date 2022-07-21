2fa0a1a3e8a1e6d31b0e988ee96746d0f12eb8b3
Bimal Shrestha <69587963+itsbimal@users.noreply.github.com>
2022-07-21 10:20:22 +0545

    Webrtc and hooks with actions

--- src/components/pages/room/Room.jsx
+++ src/components/pages/room/Room.jsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import style from './Room.module.css'
-import { getRoom } from '../../http/Index';
+
 import { useWebRTC } from '../../../hooks/useWebRTC';
+import { getRoom } from '../../../http/Index';
 
 const Room = () => {
 
--- src/components/pages/rooms/Rooms.jsx
+++ src/components/pages/rooms/Rooms.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import AddRoomModal from "../../components/addRoomModal/AddRoomModal";
-import RoomCard from "../../components/roomCard/RoomCard";
-import { getAllRooms } from "../../http/Index";
+
+import { getAllRooms } from "../../../http/Index";
+import AddRoomModal from "../addRoomModal/AddRoomModal";
+import RoomCard from "../roomCard/RoomCard";
 import style from "./Rooms.module.css";
 
 // const rooms = [
